0ecebf892b5c01d00c5b8b18bfab8bb5c0fc6011
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-03-05 14:36:30 +0530

    fix: colour side effect due to delete

--- src/components/NoteFooter.js
+++ src/components/NoteFooter.js
@@ -38,7 +38,7 @@ function NoteFooter({
                 title: "",
                 body: "",
                 label: "",
-                colour: "#fff",
+                colour: colours.Colour,
                 pinned: false,
             });
             return;
@@ -77,7 +77,7 @@ function NoteFooter({
                 >
                     <path
                         d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3H9M7 6h10v13H7V6m2 2v9h2V8H9m4 0v9h2V8h-2z"
-                        fill="rgb(136, 21, 21)"
+                        fill="rgb(204, 68, 68)"
                     ></path>
                 </svg>
             </button>
