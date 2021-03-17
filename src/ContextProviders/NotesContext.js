d3f2553b0f7a8d29daf70ee3522b8e1ec8437483
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-03-17 20:28:34 +0530

    changed the note data structure to obj of obj

--- src/ContextProviders/NotesContext.js
+++ src/ContextProviders/NotesContext.js
@@ -4,8 +4,8 @@ import colours from "../components/Helpers/Colours";
 const NotesContext = createContext();
 
 export function NotesProvider({ children }) {
-    const [pinned, setPinned] = useState([
-        {
+    const [notesList, setNotesList] = useState({
+        "7fb5f11e-b884-4854-a968-e7fd054c56e9": {
             uuid: "7fb5f11e-b884-4854-a968-e7fd054c56e9",
             title: "Books on Finance",
             body:
@@ -13,50 +13,55 @@ export function NotesProvider({ children }) {
             label: "Todo",
             colour: "rgb(172, 120, 25, 0.5)",
             pinned: true,
+            lastModified: Date.now(),
+            lastEdited: Date.now(),
         },
-    ]);
-    const [others, setOthers] = useState([
-        {
-            uuid: "e88d054c56e9-7fb5f11e-b884-4854-a968-",
+        "e88d054c56e9-7fb5f11e-b884-4854-a968": {
+            uuid: "e88d054c56e9-7fb5f11e-b884-4854-a968",
             title: "Books on Finance",
             body:
                 "Financial Affairs Of The Common Man: Master the Art of Personal Finance Management - Anil Lamba\n\nRomancing the Balance Sheet (2nd Edition) - Anil Lamba\n\nThe Two-Minute Revolution: The Art of Growing Business - Sangeeta Talwar\n\nLean Analytics: Use Data to Build a Better Startup Faster - Alistair Croll and Benjamin Yoskovitz\n\nTraction: Get A Grip On Your Business - Gino Wickman\n\nHow to Make Money in Stocks: A Winning System in Good Times Or Bad- William O'Neil",
             label: "Important",
             colour: "rgb(88, 141, 28, 0.5)",
             pinned: false,
+            lastModified: Date.now(),
+            lastEdited: Date.now(),
         },
-    ]);
+    });
 
     const [note, setNote] = useState({
         uuid: v4(),
         title: "",
         body: "",
         label: "",
-        colour: colours.Colour,
+        colour: colours.Default,
         pinned: false,
+        lastModified: Date.now(),
+        lastEdited: Date.now(),
     });
-    const [editNote, setEditNote] = useState();
+    const [editNote, setEditNote] = useState({});
 
     const [newNoteFlag, setNewNoteFlag] = useState(false);
     const [editModal, setEditModal] = useState(false);
 
     function addNote() {
-        let title = note.title.trim();
-        let body = note.body.trim();
+        const title = note.title.trim();
+        const body = note.body.trim();
         if (title.length || body.length) {
-            if (note.pinned) {
-                setPinned([note, ...pinned]);
-            } else {
-                setOthers([note, ...others]);
-            }
+            setNotesList((currentNotes) => ({
+                [note.uuid]: note,
+                ...currentNotes,
+            }));
         }
         setNote({
             uuid: v4(),
             title: "",
             body: "",
             label: "",
-            colour: colours.Colour,
+            colour: colours.Default,
             pinned: false,
+            lastModified: Date.now(),
+            lastEdited: Date.now(),
         });
         setNewNoteFlag(false);
     }
@@ -64,10 +69,8 @@ export function NotesProvider({ children }) {
     return (
         <NotesContext.Provider
             value={{
-                pinned,
-                setPinned,
-                others,
-                setOthers,
+                notesList,
+                setNotesList,
                 note,
                 setNote,
                 editNote,
